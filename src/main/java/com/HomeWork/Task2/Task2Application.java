f1221ff1e6478dcafe82318604cf1f6fcdac9442
anatolii <anatoliise@mail.ru>
2024-02-25 14:52:54 +0300

    comit

--- src/main/java/com/HomeWork/Task2/Task2Application.java
+++ src/main/java/com/HomeWork/Task2/Task2Application.java
@@ -3,8 +3,6 @@ package com.HomeWork.Task2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
 @SpringBootApplication
 public class Task2Application {
 
